77de5ec4cf50e8af89c92e66bf177285df2d905d
Daniel <danielnguyen5201@gmail.com>
2022-01-09 23:04:53 +0700

    add config for eslint

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactElement } from 'react'
+import { Button } from '@mui/material'
+
+interface ButtonProps {
+  variant: 'contained' | 'text' | 'outlined' | undefined
+}
+
+const CommonButton: FC<ButtonProps> = ({ variant = 'contained', children }) => {
+  return (
+    <Button variant={variant}>
+      {children}
+    </Button>
+  )
+}
+
+export default CommonButton
--- src/queries/index.ts
+++ src/queries/index.ts
@@ -2,6 +2,6 @@ import { getList } from 'apis'
 import { useQuery } from 'react-query'
 import { List } from 'types'
 
-export const useListCoin = () => {
+export const useList = () => {
   return useQuery<List[], Error>('list', getList)
 }
